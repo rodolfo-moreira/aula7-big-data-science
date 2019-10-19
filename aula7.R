6fec9199ebafb1f02d02064e8d7072f5979d56cc
Logon Aluno <logonaluno@local.com>
2019-10-18 22:45:08 -0300

    svm

--- aula7.R
+++ aula7.R
@@ -38,18 +38,38 @@ t <- read.csv("train.csv")
 #Tratamento dos dados
 #Preenche as idades on os campos são nulos com a média
 t[is.na(t$Age),]$Age<-mean(t[!is.na(t$Age),]$Age)
+t$Survived_f<-as.factor(t$Survived)
 
 set.seed(33)
 va<-sample(891)
 treino <- t[va[1:600],]
 teste <- t[va[601:891],]
-mod<-randomForest(Survived~Sex+Pclass+Age, data=treino)
+mod<-randomForest(Survived_f~Sex+Pclass+Age, data=treino)
 pred <- predict(mod, newdata=teste)
-pred<-ifelse(pred<.5,0,1)
+#pred<-ifelse(pred<.5,0,1)
 cbind(pred, teste$Survived)
 table(pred,teste$Survived )
 
 
+
+#SVM
+library(e1071)
+t <- read.csv("train.csv")
+
+#Tratamento dos dados
+#Preenche as idades on os campos são nulos com a média
+t[is.na(t$Age),]$Age<-mean(t[!is.na(t$Age),]$Age)
+t$Survived_f<-as.factor(t$Survived)
+
+set.seed(33)
+va<-sample(891)
+treino <- t[va[1:600],]
+teste <- t[va[601:891],]
+mod<-svm(Survived_f~Sex+Pclass+Age, data=treino)
+pred <- predict(mod, newdata=teste)
+#pred<-ifelse(pred<.5,0,1)
+cbind(pred, teste$Survived)
+table(pred,teste$Survived )
 
 
 
