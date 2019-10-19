16255cfdd2a31510779acdcc3fa8c3a5f77f48be
Logon Aluno <logonaluno@local.com>
2019-10-18 22:35:33 -0300

    Add Random Forest

--- aula7.R
+++ aula7.R
@@ -5,6 +5,9 @@ install.packages("party")
 library(party)
 install.packages("e1071")
 library(e1071)
+install.packages("randomForest")
+library(randomForest)
+
 
 
 
@@ -22,6 +25,25 @@ va<-sample(891)
 treino <- t[va[1:600],]
 teste <- t[va[601:891],]
 mod<-glm(Survived~Sex+Pclass+Age, data=treino, family=binomial())
+pred <- predict(mod, newdata=teste)
+pred<-ifelse(pred<.5,0,1)
+cbind(pred, teste$Survived)
+table(pred,teste$Survived )
+
+
+#Random Forest
+
+t <- read.csv("train.csv")
+
+#Tratamento dos dados
+#Preenche as idades on os campos são nulos com a média
+t[is.na(t$Age),]$Age<-mean(t[!is.na(t$Age),]$Age)
+
+set.seed(33)
+va<-sample(891)
+treino <- t[va[1:600],]
+teste <- t[va[601:891],]
+mod<-randomForest(Survived~Sex+Pclass+Age, data=treino)
 pred <- predict(mod, newdata=teste)
 pred<-ifelse(pred<.5,0,1)
 cbind(pred, teste$Survived)
